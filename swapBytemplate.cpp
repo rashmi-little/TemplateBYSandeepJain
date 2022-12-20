9c128a0e2273e036ac69ebbd505a1f34d690eb37
gudulu <rrnayak165@gmail.com>
2022-12-20 09:56:17 +0530

    This is program to check greatest among two number by template

---
+++ swapBytemplate.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+template <typename req>
+int swap(req x,req y)
+{
+    req temp;
+    temp=x;
+    x=y;
+    y=temp;
+    cout<<"After swapping "<<x<<" "<<y;
+    return 0;
+} 
+int main()
+{
+    swap<int>(10,15);
+    swap<char>('c','k');
+}
