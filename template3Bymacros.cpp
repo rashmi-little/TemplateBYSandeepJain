38b54c91d1a56864276296567dab9fae728a80ee
gudulu <rrnayak165@gmail.com>
2022-12-21 20:01:06 +0530

    This is a method of macros to check equality

---
+++ template3Bymacros.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#define equality(x,y) ((x==y)?1:0);
+using namespace std;
+int main()
+{
+    cout<<equality(10,20);
+    cout<<endl;
+    cout<<equality(10,10);
+    cout<<endl;
+}
